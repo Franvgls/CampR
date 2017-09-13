dad1412f09f6cd5ebbf80272a997e73439f69a1b
Francisco Velasco <francisco.velasco@st.ieo.es>
2017-09-13 12:04:22 +0200

    Incluidos los datos en nueva estructura para que estén contenidos en el paquete.

--- R/As1.R
+++ R/As1.R
@@ -5,7 +5,7 @@
 #' @docType data
 #' @title Serie histórica de la campaña ARSA de primavera
 #' @usage As1
-#' @format A vector with codes for the surveys from 1992 to 2016
+#' @format A vector with codes for the surveys from 1992 to 2017
 #' @family Series datos
 #' @family ARSA
 As1<-c(paste(1,92:99,sep=""),paste(10,c(0:2,4:9),sep=""),paste(11,0:6,sep=""))
