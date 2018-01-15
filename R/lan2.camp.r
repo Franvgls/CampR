81a9a1282eb7296771a4e04d6d553c0c07500922
Fran <franvgls@gmail.com>
2018-01-15 09:07:39 +0100

    Asignar ejemplo a lan2.camp

--- R/lan2.camp.r
+++ R/lan2.camp.r
@@ -3,9 +3,9 @@
 #' Busca y devuelve los lances especiales de una campaña 
 #' @param camp Campaña a representar en el mapa de un año comcreto (XX): Demersales "NXX", Porcupine "PXX", Arsa primavera "1XX" y Arsa otoño "2XX"
 #' @param dns Elige el origen de las bases de datos: Porcupine "Pnew", Cantábrico "Cant, Golfo de Cádiz "Arsa" (únicamente para sacar datos al IBTS, no gráficos)
+#' @examples lan2.camp("P01","Pnew")
 #' @export
 lan2.camp<-function(camp,dns) {
   if (length(camp)>1) stop("Sólo se puede seleccionar una campaña, proces individualmente")
   datlan.camp(camp,dns)[datlan.camp(camp,dns)[,3]==2,"lance"]
 }
-# lan2.camp("P01","Pnew")
