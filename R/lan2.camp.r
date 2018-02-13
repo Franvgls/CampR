dbf1f1620e64e875a80e0b9fc83c7aa78e1ffe9c
Fran <franvgls@gmail.com>
2018-02-13 10:52:37 +0100

    arregla lan2.camp

--- R/lan2.camp.r
+++ R/lan2.camp.r
@@ -2,8 +2,8 @@
 #'
 #' Busca y devuelve los lances especiales de una campaña 
 #' @param camp Campaña a representar en el mapa de un año comcreto (XX): Demersales "NXX", Porcupine "PXX", Arsa primavera "1XX" y Arsa otoño "2XX"
-#' @param dns Elige el origen de las bases de datos: Porcupine "Pnew", Cantábrico "Cant, Golfo de Cádiz "Arsa" (únicamente para sacar datos al IBTS, no gráficos)
-#' @examples lan2.camp("P01","Pnew")
+#' @param dns Elige el origen de las bases de datos: Porcupine "Porc", Cantábrico "Cant", Golfo de Cádiz "Arsa" (únicamente para sacar datos al IBTS, no gráficos)
+#' @examples lan2.camp("P01","Porc")
 #' @export
 lan2.camp<-function(camp,dns) {
   if (length(camp)>1) stop("Sólo se puede seleccionar una campaña, proces individualmente")
