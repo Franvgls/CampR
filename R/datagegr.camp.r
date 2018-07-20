6cf9028444191142ea81442f912707be7fb346fe
Fran <franvgls@gmail.com>
2018-07-20 12:56:21 +0200

    Solucionando Foo?

--- R/datagegr.camp.r
+++ R/datagegr.camp.r
@@ -14,7 +14,7 @@
 #' @return Devuelve un data.frame con campos lance, lat, long y abundancia por edad 0,1,2...Plus de edad, lance, peso número subestrato...
 #' @family edades
 #' @export
-datagegr.camp<- function(gr,esp,camp,dns="Porc",plus=8,cor.time=TRUE,n.ots=FALSE,AltAlk=AltAlk,incl2=TRUE,mediahora=1) {
+datagegr.camp<- function(gr,esp,camp,dns="Porc",plus=8,cor.time=TRUE,n.ots=FALSE,AltAlk=NA,incl2=TRUE,mediahora=1) {
   if (length(camp)>1) {stop("seleccionadas más de una campaña, no se pueden sacar resultados de m?s de una")}
   if (length(esp)>1) {
     stop("Sólo se puede incluir una especie en esta función")
@@ -35,7 +35,7 @@ datagegr.camp<- function(gr,esp,camp,dns="Porc",plus=8,cor.time=TRUE,n.ots=FALSE
     ntalls$numer<-ntalls$numer/ntalls$weight.time
     ntalls<-ntalls[,1:6]
   }
-  edad<-GetAlk.camp(gr,esp,camp,dns,plus,n.ots,AltAlk)
+  edad<-GetAlk.camp(gr,esp,camp,dns,plus,n.ots=n.ots,AltAlk=AltAlk)
   # identifica si la ALK est? hecha por sexos o conjunta
   agebysex<-ifelse(any(edad$sexo!=3),T,F)
   if (agebysex) {
