622dcfac9acc78b1d8b79a72eb15e02db25a55f1
Fran <franvgls@gmail.com>
2018-11-22 13:54:42 +0100

    Cambio de número de .5 a .6 con actualización de MeanMaxL, mejoras en CAMPtoHL con chequeo de Aphia....

--- R/CAMPtoHL.R
+++ R/CAMPtoHL.R
@@ -7,16 +7,17 @@
 #' @param inclSpecie si T incluye el nombre de la especie y el Código, si no sólo el Aphia
 #' @param quart si F deja en cada lance el valor del trimestre en que se realizó el lance, si T se deja el que tiene la campaña por defecto, 1 para Arsa 1Q, 3 para Porcupine y 4 para Arsa 4Q y Demersales Northern Shelf
 #' @param incl2 Si F deja fuera los lances especiales que actualmente no se transmiten a DATRAS, si T los incluye
+#' @param export Si T crea un fichero csv con todos los datos corregidos (APHIAs) en el directorio CAMP donde está el especies.dbf este es importable al especies.dbf con un append from deli with , quitando todos los peces grupo="1"
 #' @return Devuelve un data.table con datos de cada especie en el formato HL de DATRAS. DATRAS requiere que los datos no tengan cabecera y el trimestre sea el que corresponde a la campaña, además de no tener "". Por ello se debe pasar a fichero con la orden: write.table(CAMPtoHH(Xyy,dns),"nombrearchivo.csv",sep=",",quote=F,col.names=F,row.names=F))
 #' @examples # CAMPtoHL("P14","Porc")
 #' @export
-CAMPtoHL<-function(camp,dns,inclSpecie=F,quart=T,incl2=F) {
+CAMPtoHL<-function(camp,dns,inclSpecie=FALSE,quart=TRUE,incl2=FALSE,export=FALSE) {
     if (length(camp)>1) {stop("seleccionadas más de una campaña, no se pueden sacar resultados de más de una")}
     DB<-data.table::as.data.table(datlan.camp(camp,dns,redux=F,incl0 = F,incl2=incl2))
    	ch1<-RODBC::odbcConnect(dsn=dns)
    	RODBC::odbcSetAutoCommit(ch1, FALSE)
    	ntalls<-data.table::as.data.table(RODBC::sqlFetch(ch1,paste0("NTALL",camp)))
-    names(ntalls)<-tolower(names(ntalls))
+   	names(ntalls)<-tolower(names(ntalls))
     RODBC::odbcClose(ch1)
     ch2<-RODBC::odbcConnect(dsn="Camp")
  	  RODBC::odbcSetAutoCommit(ch2, FALSE)
@@ -24,6 +25,25 @@ CAMPtoHL<-function(camp,dns,inclSpecie=F,quart=T,incl2=F) {
  	  RODBC::odbcClose(ch2)
  	  names(especies)<-tolower(names(especies))
     especies<-subset(especies,especies$grupo==1)
+    #    especies<-subset(especies,especies$esp %in% unique(ntalls[ntalls$grupo==2,"esp"]))
+    especies<-dplyr::arrange(especies,esp)  
+    especies %>% dplyr::mutate_if(is.factor,as.character) -> especies
+    especies$especie[1]<-buscaesp(especies$grupo[1],especies$esp[1])
+    if (substr(x=especies$especie[1],start=nchar(especies$especie[1])-3,stop=nchar(especies$especie[1]))==" sp.") {
+      especies$especie[1]<-sub(" sp.","",buscaesp(especies$grupo[1],especies$esp[1]),perl=T,useBytes = T)
+    }
+    if (is.na(especies$aphia[1])) especies$aphia[1]<-worrms::wm_name2id(as.character(especies$especie[1]))
+    if (export) {
+        for (i1 in 2:nrow(especies)) {
+      if (is.na(especies$aphia[i1])) {
+        especies$especie[i1]<-buscaesp(especies$grupo[i1],especies$esp[i1])
+        if (substr(x=especies$especie[i1],start=nchar(especies$especie[i1])-3,stop=nchar(especies$especie[i1]))==" sp.") {
+          especies$especie[i1]<-sub(" sp.","",buscaesp(especies$grupo[i1],especies$esp[i1]),perl=T,useBytes = T)
+        }
+        especies$aphia[i1]<-worrms::wm_name2id(especies$especie[i1])
+      }
+    }
+    write.csv(especies,"c:/camp/peces.csv",row.names = F)}
     if (substr(dns,1,4)=="Cant" | substr(dns,1,4)=="Cnew") {
        DB$Gear="BAK"
        DB$barco=ifelse(DB$barco=="MOL","29MO",ifelse(DB$barco=="CDS","CDS"))
@@ -81,3 +101,4 @@ CAMPtoHL<-function(camp,dns,inclSpecie=F,quart=T,incl2=F) {
           }
     HL_north[order(HL_north$HaulNo,HL_north$SpecCode,HL_north$LngtClass),]               
     }
+
