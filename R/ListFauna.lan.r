3c2f067e429ce72c5a1dfeab560828f27db17b62
Fran <franvgls@gmail.com>
2020-01-23 22:22:25 +0100

    Pequeñas mejoras en viñetas. Inclusión función ListFauna.lans que saca una faunistica general de la campaña con todos los lances

--- R/ListFauna.lan.r
+++ R/ListFauna.lan.r
@@ -5,22 +5,25 @@
 #' @param dns Elige el origen de las bases de datos: Porcupine "Porc" o "Pnew", Cantábrico "Cant, Golfo de Cádiz "Arsa"
 #' @param lan Lance del que se extrae la faunística
 #' @param gr Grupo de la especie: 1 peces, 2 crustáceos 3 moluscos 4 equinodermos 5 invertebrados. Si NA, muestra todos los grupos a excepción de 6 desechos y otros no orgánicos
-#' @return Devuelve un data.frame con columnas grupo,esp,especie,peso,numero
+#' @param out salida de la función, por defecto TRUE, pero si FALSE no saca el resultado en pantalla (lo asigna a variable para ListFauna.lans)
+#' @return Devuelve un data.frame con columnas grupo,esp,lanceui,especie,peso,numero
 #' @seealso {\link{ListFauna.camps}}, {\link{ListFaunaTals.camps}}
 #' @examples ListFauna.lan("12C","Cant",1,gr=c(1,2))
 #' @export
-ListFauna.lan<- function(camp,dns="Porc",lan,gr=NA) {
+ListFauna.lan<- function(camp,dns="Porc",lan,gr=NA,out=TRUE) {
   if (length(camp)>1) {stop("seleccionadas más de una campaña, no se pueden sacar resultados de más de una")}
   lan<-format(lan,width=3,justify="r")
   ch1<-DBI::dbConnect(odbc::odbc(), dns)
   listsps<-DBI::dbGetQuery(ch1,paste0("select grupo,esp,lance,peso_gr,numero from FAUNA",camp," where lance='",lan,"'"))
   DBI::dbDisconnect(ch1)
-  if (nrow(listsps)==0) {stop(paste("Sin capturas en el lance",lan))}
+  if (nrow(listsps)==0) {message(paste("Sin capturas en el lance",lan))}
   if (any(!is.na(gr))) listsps<-listsps[listsps$grupo %in% gr,]
-  if (nrow(listsps)==0) {stop(paste("Sin capturas del grupo",gr," en el lance",lan))}
+  if (nrow(listsps)==0) {message(paste("Sin capturas del grupo",gr," en el lance",lan))}
+  if (nrow(listsps)>0){
   listsps$especie=NA
   listsps$peso=listsps$peso_gr/1000
   for (i in 1:nrow(listsps)) {
     listsps$especie[i]=buscaesp(listsps$grupo[i],listsps$esp[i]) }
-  print(setorder(listsps,grupo,-peso_gr))
+  if (out) print(setorder(listsps,grupo,-peso_gr))
+  else setorder(listsps,grupo,-peso_gr)}
   }
---
+++ R/ListFauna.lans.R
@@ -0,0 +1,22 @@
+#' Capturas de un todos los lances en una campaña
+#'
+#' Muestra un listado de las especies capturadas en peso y número en un lance concreto. Se pueden seleccionar grupos de especies
+#' @param camp Campaña de la que se extrae los datos del lance (lan) un año comcreto (XX): Demersales "NXX", Porcupine "PXX", Arsa primavera "1XX" y Arsa otoño "2XX"
+#' @param dns Elige el origen de las bases de datos: Porcupine "Porc" o "Pnew", Cantábrico "Cant, Golfo de Cádiz "Arsa"
+#' @param lan Lance del que se extrae la faunística
+#' @param gr Grupo de la especie: 1 peces, 2 crustáceos 3 moluscos 4 equinodermos 5 invertebrados 6 basura. Si NA, muestra todos los grupos
+#' @param excl.sect Sirve para excluir uno o más sectores o estratos, ponerlos como un vector por separado, i.e. c("A","B")
+#' @param incl2 Si TRUE se incuyen los lances especiales
+#' @return Devuelve un data.frame con columnas grupo,esp, lance,especie,peso,numero
+#' @seealso {\link{ListFauna.camps}}, {\link{ListFaunaTals.camps}}
+#' @examples ListFauna.lans("12C","Cant",gr=c(1,2))
+#' @export
+ListFauna.lans<- function(camp,dns="Porc",gr=NA,excl.sect=NA,incl2=FALSE) {
+  if (length(camp)>1) {stop("seleccionadas más de una campaña, no se pueden sacar resultados de más de una")}
+  lans<-datlan.camp(camp=camp,dns=dns,excl.sect = excl.sect,redux=T,incl2=incl2)
+  datos<-ListFauna.lan(camp,dns,lans$lance[1],gr=gr,out=FALSE)
+  for (i in lans$lance[2:length(lans)]) {
+    datos<-rbind(datos,ListFauna.lan(camp,dns,i,gr=gr,out=FALSE))
+  }
+  print(setorder(datos,lance,grupo,-peso_gr))
+}
